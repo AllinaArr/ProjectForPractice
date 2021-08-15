cb751218592cf69316a62ee186ba5dd987e740f8
natalliapl <82922277+natalliapl@users.noreply.github.com>
2021-08-15 14:37:33 -0400

    eatsy loh first commit

---
+++ src/test/java/com/company/pages/EatsyLog.java
@@ -0,0 +1,20 @@
+package com.company.pages;
+
+import com.company.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EatsyLog {
+
+    public EatsyLog(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "global-enhancements-search-query")
+    public WebElement searchBar;
+
+    @FindBy(xpath = "//button[@value='Search']")
+    public WebElement searchButton;
+
+}
